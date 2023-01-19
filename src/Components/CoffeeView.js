849a946aafa53c658ddd0f9d9d412e9891151ed6
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-01-19 10:58:49 +0100

    featured section functional, styling ok, not quite right, but working!

--- src/Components/CoffeeView.js
+++ src/Components/CoffeeView.js
@@ -3,16 +3,16 @@ import "./CoffeeView.css";
 
 function CoffeeView({ allCoffees, featuredSelectedCB }) {
   const [featured, setFeatured] = useState({});
-  console.log(allCoffees);
+  //   console.log(allCoffees);
 
   function handleClick(id) {
     console.log("clicked");
     let selected = allCoffees.find((item) => item.id === id);
     setFeatured(selected);
-    console.log(selected);
+    // console.log(selected.image);
     //send the information to parent component by calling the callback function
     //right now no data is passed, only function is called and the parent console.logs hello to check the connection
-    // featuredSelectedCB();
+    featuredSelectedCB();
   }
 
   return (
@@ -27,17 +27,17 @@ function CoffeeView({ allCoffees, featuredSelectedCB }) {
             </div>
           ))}
         </div>
-        {/* {featured.name && (
+        {featured.name && (
           <div id="featured">
             <div>
               <h3>Featured Coffee</h3>
-              <p>{featured.name}</p>
+              <h4>{featured.name}</h4>
               <img src={featured.image} />
             </div>
-          </div> */}
+          </div>
+        )}
       </div>
     </div>
   );
 }
-
 export default CoffeeView;
