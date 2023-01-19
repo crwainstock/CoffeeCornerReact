8daa2fe024af95ce8e5b71939dadbc494070e9c7
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-01-19 10:09:13 +0100

    update CoffeeView to include id element

--- src/Components/CoffeeView.js
+++ src/Components/CoffeeView.js
@@ -1,17 +1,17 @@
 import { useState } from "react";
-import "../App";
+import "./CoffeeView.css";
 
 function CoffeeView({ allCoffees, featuredSelectedCB }) {
   const [featured, setFeatured] = useState({});
   console.log(allCoffees);
 
-  function handleClick() {
+  function handleClick(id) {
     console.log("clicked");
-    // let selected = allCoffees.find((item) => item.id === id);
-    // setFeatured(selected);
+    let selected = allCoffees.find((item) => item.id === id);
+    setFeatured(selected);
     //send the information to parent component by calling the callback function
     //right now no data is passed, only function is called and the parent console.logs hello to check the connection
-    // featuredSelectedCB();
+    featuredSelectedCB();
   }
 
   return (
@@ -19,10 +19,10 @@ function CoffeeView({ allCoffees, featuredSelectedCB }) {
       <h2>Pick a favorite:</h2>
       <div id="container-grid">
         <div id="grid">
-          {allCoffees.map((coffee, index) => (
-            <div key={index} onClick={() => handleClick(coffee)}>
-              <p id={index}>{coffee}</p>
-              {/* <img src={coffee.img} onClick={() => handleClick(coffee.id)} /> */}
+          {allCoffees.map((coffee) => (
+            <div key={coffee.id}>
+              <p id={coffee.id}>{coffee.name}</p>
+              <img src={coffee.image} onClick={() => handleClick(coffee.id)} />
             </div>
           ))}
         </div>
@@ -31,7 +31,7 @@ function CoffeeView({ allCoffees, featuredSelectedCB }) {
             <div>
               <h3>Featured section</h3>
               <p>{featured.name}</p>
-              <img src={featured.img} />
+              <img src={featured.image} />
             </div>
           </div>
         )}
