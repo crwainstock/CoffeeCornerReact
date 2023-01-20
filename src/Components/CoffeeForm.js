5cb33e51c5f72eec8179f13eac131c1948a7a625
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-01-20 10:59:09 +0100

    trying to add the handleSubmit function and connect it to the form on CoffeeForm component -- not complete

--- src/Components/CoffeeForm.js
+++ src/Components/CoffeeForm.js
@@ -2,13 +2,25 @@ import { useState } from "react";
 import "../App";
 
 function CoffeeForm() {
+  const [coffeeInfo, addCoffee] = useState({});
+
+  function handleSubmit(coffeeInfo) {
+    console.log("submitted");
+    let newCoffee = {
+      name: { name },
+      price: { price },
+      image: { image },
+      id: { id },
+    };
+  }
+
   return (
     <div>
       <h2>Add a coffee</h2>
       <form>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Price" />
-        <input type="url" placeholder="Image URL" />
+        <input type="text" value="name" placeholder="Name" />
+        <input type="text" value="price" placeholder="Price" />
+        <input type="url" value="image" placeholder="Image URL" />
 
         <button type="submit">Add Coffee</button>
       </form>
